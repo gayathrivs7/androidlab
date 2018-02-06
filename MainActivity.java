6fd52de36ad5ff0c4a71baf09507c5364a9ceb33
gayathrivs7 <34839679+gayathrivs7@users.noreply.github.com>
2018-02-06 09:35:23 +0530

    Add files via upload
    W2

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,11 @@
-package com.example.mcaguest.sms;
+package com.example.mcaguest.w2;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.SmsManager;
+import android.telephony.gsm.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,59 +13,93 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
-    Button send;
-    EditText ph,msg;
-    String message,phone;
+    Button button2,button3;
+    private Context context = this;
+    EditText editText2,editText3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        send = (Button) findViewById(R.id.button);
-        ph = (EditText) findViewById(R.id.editText);
-        msg= (EditText) findViewById(R.id.editText2);
-
-        send.setOnClickListener(new View.OnClickListener() {
+        button3=(Button)findViewById(R.id.button2);
+        editText2=(EditText)findViewById(R.id.editText2);
+        editText3=(EditText)findViewById(R.id.editText3);
+        button2=(Button)findViewById(R.id.button);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                AlertDialog.Builder alert= new AlertDialog.Builder(context);
+                alert.setTitle("Close application");
+                alert.setMessage("Do you want to close ? ").setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        MainActivity.this.finish();
 
-                sendSMSMessage();
+                    }
+                }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                AlertDialog ale=alert.create();
+                ale.show();
             }
         });
 
-    }
-    protected void sendSMSMessage() {
-        phone= ph.getText().toString();
-        message = msg.getText().toString();
+        // DISPLAYING ALERT BOX WITHOUT BUTTON CLICK "TOUCH"
+   /*     AlertDialog.Builder alert= new AlertDialog.Builder(context);
+        alert.setTitle("close");
+        alert.setMessage("Do you want to close ? ").setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                MainActivity.this.finish();
 
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.SEND_SMS)) {
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.SEND_SMS},
-                        MY_PERMISSIONS_REQUEST_SEND_SMS);
             }
-        }
+        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
 
-    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage(phone, null, message, null, null);
-                    Toast.makeText(getApplicationContext(), "SMS sent.",
-                            Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(),
-                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
-                    return;
-                }
             }
-        }
+        });
+        AlertDialog ale=alert.create();
+        ale.show();
+
+        */
 
     }
+    public  void send(View v) {
+        String message = editText3.getText().toString();
+        String number = editText2.getText().toString();
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            //smsManager.sendTextMessage(number, null, message, null, null);
+            smsManager.sendTextMessage(message,null,number,null,null);
+            Toast.makeText(context, "MESSAGE SENT", Toast.LENGTH_SHORT).show();
+
+
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(getApplicationContext(),
+                    "SMS faild, please try again later!",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+
+        }
+    }
+
 }
+/*try {
+				SmsManager smsManager = SmsManager.getDefault();
+				smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+				Toast.makeText(getApplicationContext(), "SMS Sent!",
+							Toast.LENGTH_LONG).show();
+			  } catch (Exception e) {
+				Toast.makeText(getApplicationContext(),
+					"SMS faild, please try again later!",
+					Toast.LENGTH_LONG).show();
+				e.printStackTrace();
+			  }
 
-
+			}*/
